09f4d1523a69e37ff9de386d4818abc04a8304ea
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2025-02-05 17:38:01 +0000

    update

---
+++ week03/prefixSumCode/prefixSum.h
@@ -0,0 +1,8 @@
+#ifndef __PREFIX_SUM_H
+#define __PREFIX_SUM_H
+
+
+int prefix_sum(int array[], int last); 
+bool non_negative_prefix_sum(int array[], int length);
+
+#endif
---
+++ week03/prefixSumCode/unitTestPrefixSum.cpp
@@ -0,0 +1,25 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+/*
+  DOCTest unit tests
+ */
+
+#include "../include/doctest.h"
+#include "prefix_sum.h"
+
+int nums_0[] = {};
+int nums_1[] = {-5};
+int nums_2[] = {35};
+int nums_3[] = {2,-3,3,2};
+int nums_4[] = {1,1,-1,1};
+int nums_5[] = {-1,-1,-1,-1,1,1};
+
+
+
+TEST_CASE("Test week1/2 functions") {
+  SUBCASE("non-negative prefix sums") {
+
+  };
+  SUBCASE("non-positive prefix sums") {
+
+  };
+}
