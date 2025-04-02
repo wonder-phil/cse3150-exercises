c2770573c72e1b1d21d324eb106c72a765f44bf1
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2025-04-02 17:23:43 +0000

    correct typo: -\-d{4} to -\d{4}

--- week10/exercise2/postalPattern.cpp
+++ week10/exercise2/postalPattern.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main() {
     string input = "CT 06902-1234";
-    regex word_regex{"(\\w{2}\\s*\\d{5}(-\\-d{4})?)"};
+    regex word_regex{"(\\w{2}\\s*\\d{5}(-\\d{4})?)"};
 
     auto words_begin = sregex_iterator(input.begin(), input.end(), word_regex);
     auto words_end = sregex_iterator();
