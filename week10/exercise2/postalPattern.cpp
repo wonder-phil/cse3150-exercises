1eae256c023c827122e1d10f30b34e9a9abe0f84
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2025-04-02 17:22:18 +0000

    add standard regex patterns

---
+++ week10/exercise2/postalPattern.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <regex>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string input = "CT 06902-1234";
+    regex word_regex{"(\\w{2}\\s*\\d{5}(-\\-d{4})?)"};
+
+    auto words_begin = sregex_iterator(input.begin(), input.end(), word_regex);
+    auto words_end = sregex_iterator();
+
+    cout << "Words found:\n";
+    for (sregex_iterator i = words_begin; i != words_end; ++i) {
+        cout << i->str() << endl;
+    }
+
+    return 0;
+}
---
+++ week10/exercise2/regex.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <regex>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string input = "Hello, world! This is C++: regex_test.";
+    regex word_regex{"\\b\\w+\\b"};
+
+    auto words_begin = sregex_iterator(input.begin(), input.end(), word_regex);
+    auto words_end = sregex_iterator();
+
+    cout << "Words found:\n";
+    for (sregex_iterator i = words_begin; i != words_end; ++i) {
+        cout << i->str() << endl;
+    }
+
+    return 0;
+}
+
